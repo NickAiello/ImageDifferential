de526ea6c4bd4a498f02d75f11f9b327226f9cb3
NickAiello <nick.n.aiello@gmail.com>
2019-06-08 12:14:29 -0700

    Update comparison to compare bit by bit

--- bitmap-cmp.c
+++ bitmap-cmp.c
@@ -3,52 +3,63 @@
 
 int main(int argc, char* argv[]){
 	if (argc != 3){
-		printf("Needs 3 args. You gave %d. [infile1] [infile2]", argc);
+		printf("Needs 2 args. You gave %d. [infile1] [infile2]\n", argc-1);
 	}
-	const int SIZE = 100;
-	const int BUFFSIZE = 10000;
-
-	char file1[SIZE];
-	char file2[SIZE];
+	const int SIZE = 100, BUFFSIZE = 10000;
+	char file1[SIZE], file2[SIZE];
 	sprintf(file1,"%s",argv[1]);
 	sprintf(file2,"%s",argv[2]);
-	unsigned char buf1[BUFFSIZE];
-	unsigned char buf2[BUFFSIZE];
-	FILE *f1;
-	FILE *f2;
-	int n,m,i,offset;
+	unsigned char buf1[BUFFSIZE], buf2[BUFFSIZE];
+	FILE *f1, *f2;
+	int n,m,i,j,offset;
+
+	//starting offset of pixel data (for .bmp format)
 	offset = 1078;
+
+	//open input files
 	f1 = fopen(file1,"rb");
 	f2 = fopen(file2, "rb");
 
 	//seek files to the bmp standard for pixel data start
 	fseek(f1,offset,SEEK_SET);
 	fseek(f2,offset,SEEK_SET);
+	
+	//Counters for stats
+	unsigned long int sameBytes, diffBytes, totalBytes, sameBits, diffBits;
+	totalBytes = sameBytes = diffBytes = sameBits = diffBits = 0;
 
-	unsigned int sameCount, diffCount, totalBytes, sameCountXORCheck, diffCountXORCheck;
-	totalBytes = sameCount = diffCount = sameCountXORCheck = diffCountXORCheck = 0;
-	//loop through the file pixel data comparing counts of same bytes
-	while (n = fread(buf1,1,BUFFSIZE,f1)){
-		m = fread(buf2,1,BUFFSIZE,f2);
-		if (m == 0 || n == 0){
+	//loop through the file pixel data comparing counts of same bits / bytes
+	while (n = fread(buf1,1,BUFFSIZE,f1)){//read from file 1
+		m = fread(buf2,1,BUFFSIZE,f2);//read from file 2
+		if (m == 0 || n == 0){//if the amount read is different, file size is different, one file has ended, quit here
 			break;
 		}
 		totalBytes += n;
 		for(i=0;i<n;i++){
-			if (buf1[i] == buf2[i]){ sameCount++; }
-			else { diffCount++; }
-			if ((unsigned int)(buf1[i] ^ buf2[i]) == 0) {
-				sameCountXORCheck++; 	
+			//get the # of bits same / diff
+			unsigned char compareIsolater = 0b00000001;
+			for (j=0;j<8;j++){
+				if ((compareIsolater & (unsigned char)buf1[i]) == (compareIsolater & (unsigned char)buf2[i])) {
+					sameBits++;
+				} else {
+					diffBits++;
+				}
+				compareIsolater = compareIsolater << 1;
 			}
-			else { diffCountXORCheck++; }
-			//printf("%d ^ %d = %d\n",(unsigned int)buf1[i],(unsigned int)buf2[i], (unsigned int)(buf1[i]^buf2[i]));
+			//get # of bytes same / diff
+			if (buf1[i] == buf2[i]){ sameBytes++; }
+			else { diffBytes++; }
+			//printf("%d ^ %d = %d\n",(unsigned int)buf1[i],(unsigned int)buf2[i], (unsigned int)(buf1[i]^buf2[i]));//test print for finding 
 		}
 	}
-	printf("# of same bytes = %d vs %d\n",sameCount, sameCountXORCheck);
-	printf("# of diff bytes = %d vs %d\n",diffCount, diffCountXORCheck);
-	printf("Total Bytes = %d\n",totalBytes);
-	printf("Percent same = %f vs %f\n",((double)sameCount / (double)totalBytes)*100, ((double)sameCountXORCheck / (double)totalBytes)*100);
-	printf("Percent diff = %f vs %f\n",((double)diffCount / (double)totalBytes)*100, ((double)diffCountXORCheck / (double)totalBytes)*100);
+
+	//Print stats
+	printf("# of same bytes / bits = %ld / %ld\n",sameBytes,sameBits);
+	printf("# of diff bytes / bits = %ld / %ld\n",diffBytes, diffBits);
+	printf("Total bytes / bits = %ld / %ld\n",totalBytes,totalBytes*8);
+	printf("Percent same bytes / bits = %f / %f\n",((double)sameBytes / (double)totalBytes)*100,((double)sameBits / ((double)totalBytes*8))*100);
+	printf("Percent diff bytes / bits = %f / %f\n",((double)diffBytes / (double)totalBytes)*100,((double)diffBits / ((double)totalBytes*8))*100);
+
 	//close files
 	fclose(f1);
 	fclose(f2);
